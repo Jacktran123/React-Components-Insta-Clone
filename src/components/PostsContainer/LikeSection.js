7d0d961ba535eb30af70a103fb7539c0b30a56a5
Khoa Tran <jacktran1799@gmaail.com>
2020-02-04 13:10:38 -0600

    Finished project

--- src/components/PostsContainer/LikeSection.js
+++ src/components/PostsContainer/LikeSection.js
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const LikeSection = props => {
-  return (
-    <div>
-    <div
-      className="like-section"
-      key="likes-icons-container"
-    >
-      <div className="like-section-wrapper">
-        <i className="far fa-heart" />
-      </div>
-      <div className="like-section-wrapper">
-        <i className="far fa-comment" />
-      </div>
-    </div>
-    <p className="like-number">
-      
-      likes</p>
-</div>
-  )
+
+
+const LikeSection = () => {
+  const [postLikes, setPostLikes]= useState(0);
+  const [comments, setComments]= useState(0);
+	const clickHandler = () => setPostLikes(postLikes + 1);
+
+	return (
+		<div>
+			<div className="like-section" key="likes-icons-container">
+				<div className="like-section-wrapper">
+					<i className="far fa-heart" onClick={clickHandler} />
+				</div>
+				<div className="like-section-wrapper">
+					<i className="far fa-comment" />
+				</div>
+			</div>
+			<p className="like-number">{`${postLikes} `} likes</p>
+     
+		</div>
+	);
 };
 
 export default LikeSection;
--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,16 +1,21 @@
 //Complete the necessary code in this file
 import React from "react";
 import Post from "./Post";
+
 import "./Posts.css";
 
 // pass the data from App.js down as props then map through the data
-const PostsPage = () => {
-  return (
-    <div className="posts-container-wrapper">
-      {/* map through data here */}
-    </div>
-  );
-};
+const PostsPage = ({ data }) => {
+   
 
+	return (
+		<div className="posts-container-wrapper">
+			{/* map through data here */}
+			{data.map((post, index) => (
+				<Post post={post} key={index} />
+			))}
+		</div>
+	);
+};
 export default PostsPage;
 
